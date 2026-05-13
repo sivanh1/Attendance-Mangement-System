6aecd980e6673dbdf79a568f28a042b2209b1f35
Sivanesh <sivanesh384@gmail.com>
2026-05-13 20:07:26 +0530

    Delete attendance directory

--- attendance/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SystemSettings, Attendance, LeaveRequest, WFHRequest
-
-admin.site.register(SystemSettings)
-admin.site.register(Attendance)
-admin.site.register(LeaveRequest)
-admin.site.register(WFHRequest)
